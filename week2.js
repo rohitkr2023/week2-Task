187fd19609584dbb5b1442ee5415da198e22a4b0
ROHIT KUMAR <156569033+rohitkr2023@users.noreply.github.com>
2025-02-27 14:15:17 +0530

    Add files via upload

---
+++ week2.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navbarLinks = document.querySelectorAll(".navbar a");
+
+    navbarLinks.forEach(link => {
+        link.addEventListener("click", event => {
+            event.preventDefault();
+            const targetId = link.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop - 50,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
